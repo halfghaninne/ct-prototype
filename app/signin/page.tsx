8ea50ea9a8e5dff94d3947372a85b9033db9b702
Alexandra Millatmal <alexandrajmh@gmail.com>
2026-05-21 13:56:59 -0400

    style: toggle hide/show password for sign in, not working

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -8,18 +8,42 @@ import styles from "./ui.module.css"
  
 export default function SigninForm() {
   const [state, action, pending] = useActionState(signin, undefined)
- 
+  // let passInput = HTMLInputElement;
+  // let image = HTMLImageElement;
+  if (typeof window !== 'undefined') {
+    const passInput = document.getElementById('password') as HTMLInputElement;
+    const image = document.getElementById('image') as HTMLImageElement;
+
+    function toggleImage() {
+    if (passInput && image) {
+      if (image.src.endsWith('/eye-show.svg')) {
+        passInput.type = 'text';
+        image.src = '/eye-hide.svg';
+      } else {
+        passInput.type = 'password';
+        image.src = '/eye-show.svg';
+      }   
+    }
+  }
+  }
+  
   return (
     <form className={styles.center} action={action}>
       <div>
         <label htmlFor="email">Email</label>
-        <input id="email" name="email" placeholder="Email" />
+        <input className={styles.input} id="email" name="email" placeholder="Email" />
       </div>
       {/* {state?.errors?.email && <p>{state.errors.email}</p>} */}
  
       <div>
         <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" />
+        <input className={styles.input} id="password" name="password" type="password" />
+        <img
+          id="image"
+          onClick={() => toggleImage()}
+          style={{color: 'white', height: '25px', display: 'inline-block'}}
+          src="/eye-show.svg" 
+        />
       </div>
       {/* {state?.errors?.password && (
         <div>
