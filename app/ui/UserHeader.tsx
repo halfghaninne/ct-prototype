4dd74fe083ebca843b9ec6cdabdf04a46d10d04a
Alexandra Millatmal <alexandrajmh@gmail.com>
2026-05-21 01:27:27 -0400

    style: undoes some of the layout boilerplate

--- app/ui/UserHeader.tsx
+++ app/ui/UserHeader.tsx
@@ -2,11 +2,13 @@
 
 import Link from "next/link";
 import { signout } from "../actions/auth";
+import styles from './ui.module.css';
 
 export default function UserHeader({user}: {user: Boolean}) {
 
     return (
-        <div>
+        <div className={styles.headerFlex}>
+            <Link href='/'>Home</Link>
             { user ? <Link href='/' onNavigate={(e) => {signout();}}>Sign Out</Link> : <Link href="/signin">Sign In</Link>}
         </div>
     )
